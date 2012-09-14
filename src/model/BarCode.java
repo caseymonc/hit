74cf252c65a5baac3eef5c462f9bfc8a5b749637
David Mathis <flashmathis@gmail.com>
2012-09-14 07:00:41 -0600

    Updated BarCode class and added a .gitignore file

--- src/model/BarCode.java
+++ src/model/BarCode.java
@@ -2,14 +2,34 @@ package model;
 
 import java.io.Serializable;
 
+/** BarCode
+ * A Barcode must be a valid UPC-A barcode.  All products 
+ * will have their own BarCode and HIT will generate new 
+ * unique BarCodes for all items.
+ * 
+ * @author casey dmathis
+ */
 public class BarCode implements Serializable {
+	
+	/** The value of the BarCode.
+	 * This value must be a valid UPC-A barcode and must
+	 * be unique for all products and items in the system.
+	 */
 	private String barCode;
 	
+	/** Constructor
+	 * 
+	 * @param barCode - The value that will be assigned to barCode. 
+	 */
 	public BarCode(String barCode){
-		
+		this.barCode = barCode;
 	}
 	
-	public String generate(){
+	/** gets the value of the BarCode
+	 * 
+	 * @return barCode - the value of the BarCode
+	 */
+	public String getBarCode(){
 		return barCode;
 	}
 }
