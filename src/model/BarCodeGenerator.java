ca39aeed009c1e5c4520c348707bfe6e173c13e1
Casey <cmoncur@solutionstream.com>
2012-09-17 17:00:33 -0600

    Added BarCodeGenerator.java

---
+++ src/model/BarCodeGenerator.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Generates Valid UPC-A BarCodes
+ * @author Casey Moncur
+ *
+ */
+public class BarCodeGenerator {
+	
+	/** Singleton instance of BarCodeGenerator*/
+	private static BarCodeGenerator instance;
+	
+	/** The set that contains all used bar codes*/
+	private Set<BarCode> barCodes;
+	
+	/**
+	 * Constructor
+	 */
+	private BarCodeGenerator(){
+		barCodes = new HashSet<BarCode>();
+	}
+	
+	/**
+	 * Get a unique bar code
+	 * @return A unique BarCode
+	 */
+	public BarCode generateBarCode(){
+		return null;
+	}
+	
+	/** 
+	 * Get an instance of BarCodeGenerator
+	 * @return singleton instance of BarCodeGenerator
+	 */
+	public static BarCodeGenerator instance(){
+		if(instance != null)
+			instance = new BarCodeGenerator();
+		return instance;
+	}
+}
