3cda7759aa42c05ff398ddf283eacbe73d271039
David Patty <davidpatty@gmail.com>
2012-09-19 16:48:06 -0600

    removed Root Unit

--- src/model/CoreObjectModel.java
+++ src/model/CoreObjectModel.java
@@ -5,7 +5,6 @@
 package model;
 
 import model.entities.BarCode;
-import model.entities.RootUnit;
 import model.entities.Item;
 import model.entities.Product;
 import model.persistence.PersistentItem;
@@ -21,21 +20,6 @@ import model.persistence.PersistentItem;
  */
 public class CoreObjectModel implements PersistentItem{
 	
-	/**
-	 *  This is the instance of the Root of the product tree
-	 */
-	private RootUnit rootUnit;
-	
-	/** 
-	 *  Holds all of the products in the program
-	 */
-	private ProductMap allTheProducts;
-	
-	/** 
-	 *  Holds all of the items in the program
-	 */
-	private ItemMap allTheItems;
-
 	/** finds and returns the product associated with the unique barcode
 	 * 
 	 * @param barcode unique for each product
@@ -43,7 +27,7 @@ public class CoreObjectModel implements PersistentItem{
 	 */
 	public Product findProductByBarCode(BarCode barcode) {
 		
-		return allTheProducts.getProductByBarCode(barcode);
+		return productManager.getProductByBarCode(barcode);
 	}
 	
 	/** finds and returns the Item associated with the unique barcode
