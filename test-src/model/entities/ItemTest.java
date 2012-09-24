76a3c1a943faf0bca52fa2d2aa2891efd5c2be61
Shane Dooley <dooley.shanek@byu.edu>
2012-09-24 17:26:25 -0600

    Wrote isValid method on BarCode.java. Some item tests.

--- test-src/model/entities/ItemTest.java
+++ test-src/model/entities/ItemTest.java
@@ -4,6 +4,8 @@
  */
 package model.entities;
 
+import java.util.Date;
+import model.BarCodeGenerator;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,6 +21,9 @@ import model.managers.ItemManager;
 public class ItemTest {
 	
 	ItemManager IM;
+	StorageUnit unit;
+	ProductGroup group;
+	Product product;
 	
 	public ItemTest() {
 		IM = new ItemManager();
@@ -39,9 +44,40 @@ public class ItemTest {
 	@After
 	public void tearDown() {
 	}
-	// TODO add test methods here.
-	// The methods must be annotated with annotation @Test. For example:
-	//
-	// @Test
-	// public void hello() {}
+	
+	//Barcode	Must be a valid UPC barcode
+	@Test
+	public void testValidBarcode() 
+	{
+		unit = new StorageUnit("Unit 1");
+		group = new ProductGroup("Group 1", unit, new Size(Unit.gallons, 5));
+		
+		product = new Product("Product", BarCodeGenerator.getInstance().generateBarCode(),
+				0, 0, new Size(Unit.count, 1));
+
+		Item item = new Item(BarCodeGenerator.getInstance().generateBarCode()
+				, new Date(), new Date(), product, unit);
+		
+		Item item2 = new Item(BarCodeGenerator.getInstance().generateBarCode()
+				, new Date(), new Date(), product, unit);
+		assertTrue(item.getBarCode().isValid());
+		assertTrue(item2.getBarCode().isValid());
+	}
+	
+	
+	@Test
+	public void test()
+	{
+		
+	}
+	
+
+	//Entry Date	Must be non‐empty.
+	//Entry Date	Cannot be in the future
+	//Entry Date	Cannot be prior to 1/1/2000
+	//Exit Time	This attribute is defined only if the Item has been removed from storage.
+	//Exit Time	Cannot be in the future or prior to 12 AM on the Item’s Entry Date
+	//Expiration Date	This attribute is defined only if the Product’s Shelf Life attribute has been specified.
+	//Container	Empty if the Item has been removed from storage.
+	//Container	Non‐empty if the Item has not been removed from storage. (Before it is removed, an Item is contained in one Product Container. After it is removed, it is contained in no Product Containers.)
 }
