33d9e4609fb45ccacc0d0fa431892af128708676
David Patty <davidpatty@gmail.com>
2012-09-25 17:06:59 -0600

    Added test for Adding items to correct place(where product is located)

--- test-src/model/controllers/ItemControllerTest.java
+++ test-src/model/controllers/ItemControllerTest.java
@@ -13,14 +13,18 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import model.BarCodeGenerator;
-import model.entities.BarCode;
-import model.entities.Item;
-import model.entities.Product;
-import model.entities.ProductGroup;
-import model.entities.Size;
-import model.entities.StorageUnit;
-import model.entities.Unit;
+import model.*;
+//import model.BarCodeGenerator;
+import model.entities.*;
+import model.managers.*;
+import model.controllers.*;
+
+//import model.entities.Item;
+//import model.entities.Product;
+//import model.entities.ProductGroup;
+//import model.entities.Size;
+//import model.entities.StorageUnit;
+//import model.entities.Unit;
 
 /**
  *
@@ -73,10 +77,9 @@ public class ItemControllerTest {
 	//             an Item is contained in one Product Container. After it is removed, it is contained in no Product Containers.)
 
 	
-	 @Test
-	 public void ItemHasValidEntryDate() {
-//		 BarCode barCode, Date entryDate, Date expirationDate, 
-//			Product product, ProductContainer container
+	@Test
+	public void ItemHasValidEntryDate() {
+
 		//test empty date
 		assertFalse(Item.canCreate(b, null, new Date(), p, g));
 
@@ -84,34 +87,75 @@ public class ItemControllerTest {
 		Calendar c = Calendar.getInstance();
 		c.set(2014,1,1);
 		assertFalse(Item.canCreate(b, c.getTime(), null, p, g));
-		
+
 		//Entry Date	Cannot be prior to 1/1/2000
 		c.set(1999,1,1);
 		assertFalse(Item.canCreate(b, c.getTime(), null, p, g));
-		
+
 		//all is valid
 		c.set(2002,1,1);
 		assertTrue(Item.canCreate(b, c.getTime(), null, p, g));
-		
-	 } 
-	 
-	 @Test
-	 public void ItemHasValidExitDate() {
-		 Item i = new Item(b, new Date(), null, p, g);
-		 i.remove();
-		 assertTrue(i.hasValidExitDate());
-	 }
-	 
-	 
-	 //Container	Empty if the Item has been removed from storage.
+
+	} 
+
+	@Test
+	public void ItemHasValidExitDate() {
+		Item i = new Item(b, new Date(), null, p, g);
+		i.remove();
+		assertTrue(i.hasValidExitDate());
+	}
+
+
+	//Container	Empty if the Item has been removed from storage.
 	//Container	Non-empty if the Item has not been removed from storage. (Before it is removed, 
-	 //             an Item is contained in one Product Container. After it is removed, it is contained
-	 //             in no Product Containers.)
-	 @Test
-	 public void ItemHasValidContainer() {
+	//             an Item is contained in one Product Container. After it is removed, it is contained
+	//             in no Product Containers.)
+	@Test
+	public void ItemHasValidContainer() {
 		Item i = new Item(b, new Date(), null, p, g);
 		assertFalse(i.getContainer() == null);
 		i.remove();
 		assertTrue(i.getContainer() == null);
-	 }
+	}
+	/**
+	 *  When a new Item is added to the system, it is placed in a particular Storage Unit 
+	 *  (called the “target Storage Unit”). The new Item is added to the same Product Container
+	 *  that contains the Item’s Product within the target Storage Unit. If the Item’s Product 
+	 *  is not already in a product Container within the target Storage Unit, the Product
+	 *  is placed in the Storage Unit at the root level.
+	 */	 
+	@Test
+	public void ItemAddsToCorrectPlace() {
+		CoreObjectModel COM = CoreObjectModel.getInstance();
+		StorageUnitController SC = COM.getStorageUnitController();
+		ProductGroupController PGC = COM.getProductGroupController();
+		ItemController IC = COM.getItemController();
+		
+		
+		StorageUnit rootUnit = new StorageUnit("SUnit");
+		SC.addStorageUnit(rootUnit);
+		ProductGroup destination1 = new ProductGroup("ContainsProduct",rootUnit, new Size(Unit.count, 2));
+		PGC.addProductGroup(destination1, rootUnit);
+		destination1.addProduct(p);
+		rootUnit.setProductForContainer(p, destination1);
+		
+		Item i = new Item(b, new Date(), null, p, null);
+		IC.addItem(i,rootUnit);
+		assertFalse(i.getContainer() == rootUnit);//why does this fail?
+		assertFalse(rootUnit.getAllProducts().contains(p));
+		assertTrue(i.getContainer() == destination1);
+		
+		BarCode b2 = BarCodeGenerator.getInstance().generateBarCode();
+		Product p2 = new Product("NewProduct", BarCodeGenerator.getInstance().generateBarCode(),0,5,new Size(Unit.quarts, 5));
+		Item i2 = new Item(b2, new Date(), null, p2, rootUnit);
+		rootUnit.setProductForContainer(p2,rootUnit);
+
+		//should be in the destination storage unit
+		IC.addItem(i2, rootUnit);
+		assertTrue(i2.getContainer() == rootUnit);
+		assertTrue(rootUnit.getAllProducts().contains(p2));
+		assertFalse(i2.getContainer() == destination1);
+	}
+	
+	
 }
