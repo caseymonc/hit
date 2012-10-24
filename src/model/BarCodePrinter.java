545deeb912821f2db2646ef0e02ec0733b71e46d
David Mathis <flashmathis@gmail.com>
2012-10-23 20:36:20 -0600

    Refactored the BarCodePrinter class

--- src/model/BarCodePrinter.java
+++ src/model/BarCodePrinter.java
@@ -6,10 +6,10 @@ package model;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.Barcode;
 import com.itextpdf.text.pdf.BarcodeEAN;
@@ -22,95 +22,104 @@ import gui.item.ItemData;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 import model.entities.Item;
 
 /**
- *
- * @author davidpatty
+ * Class that manages BarCode printing.
  */
 public class BarCodePrinter {
-	
+
 	/**
-	 *  When constructed this class iterates through all the added items and adds their barcodes 
-	 * to a pdf that is then displayedon the screen.
-	 * 
-	 * @param newItemBarCodes
+	 * When constructed, this class creates a PDF document that contains the BarCodes 
+	 * of all of the Items in items. After this PDF is created it is opened and displayed.
+	 *
+	 * @param items - a list of Items whose BarCodes need to be printed.
 	 */
-	public BarCodePrinter(List<ItemData> items) {
-			Random random = new Random();
+	public BarCodePrinter(List<ItemData> items) {		
+		Random random = new Random();
+		String file_name = "labels-" + Integer.toString(random.nextInt()) + ".pdf";
+		
+		if (items.size() < 1) {
+			return;
+		}
+		
+		try {	
+			// Create the PDF document
+			Document document = new Document(new Rectangle(340, 468));
+			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file_name));
 			
-		if(items.size() > 0) {
-			try {
-				//Print the newItem barcodes to a pdf			
-				BarcodeEAN codeEAN = new BarcodeEAN();
-				codeEAN.setCodeType(Barcode.UPCA);
-				Document document = new Document(new Rectangle(340, 842));
-								String file_name = "labels-" + 
-										Integer.toString(random.nextInt()) + ".pdf";
-				PdfWriter writer = PdfWriter.getInstance(document, 
-										new FileOutputStream(file_name));
-				document.open();
-				PdfContentByte cb = writer.getDirectContent();
+			document.open();
+			PdfPTable table = buildTable(items, writer);
+			document.add(table);
+			document.close();
+			
+			// Display the PDF
+			java.awt.Desktop.getDesktop().open(new File(file_name));
+		} catch (DocumentException e) {
 
-								int numCols = 6;
-								int colWidth = 200;
-								PdfPTable table = new PdfPTable(numCols);
-								table.setTotalWidth(numCols*colWidth);
-								
-								Font font = new Font(FontFamily.HELVETICA, 4);
+			System.out.println("Error: unable to create pdf: " + e.getMessage());
 
-				for(int i=0; i < items.size(); ++i)
-				{
-									Item item = (Item)items.get(i).getTag();
-									String title = item.getProduct().getDescription();
-									String entry = DateUtils.formatDate(item.getEntryDate()); 
-									String exp;
-									
-									PdfPCell cell = new PdfPCell();
-									cell.setBorder(Rectangle.NO_BORDER);
-									if(item.getExpirationDate() != null){
-										exp = " exp " + DateUtils.formatDate(item.getExpirationDate());
-									} else {
-										exp = "";
-									}
+		} catch (IOException e) {
 
-									codeEAN.setCode(item.getBarCode().toString());
-									cell.addElement(new Paragraph(title, font));
-									cell.addElement(new Paragraph(entry + exp, font));
-									
-									Image image = codeEAN.createImageWithBarcode(cb, null, null);
-									image.setSpacingBefore(3);
-									cell.addElement(image);
-									
-									table.addCell(cell);
-				}
-								
-								int remainingCells = numCols - (items.size() % numCols);
-								
-								for(int i = 0; i < remainingCells; i++) {
-									PdfPCell cell = new PdfPCell();
-									cell.setBorder(Rectangle.NO_BORDER);
-									table.addCell(cell);
-								}
-								
-								document.add(table);
-				document.close();
-				//This will allow you to open a pdf and display it on the screen
-				java.awt.Desktop.getDesktop().open(new File(file_name));
+			System.out.println("Error: unable to write to pdf: " + e.getMessage());
 
-
-			} catch(DocumentException e) {
-
-				System.out.println("Error: unable to create pdf: " + e.getMessage());
-
-			} catch(IOException e) {
-
-				System.out.println("Error: unable to write to pdf: " + e.getMessage());			
-
-			}
 		}
 	}
 	
+	private PdfPTable buildTable(List<ItemData> items, PdfWriter writer){	
+		int numCols = 6;
+		int colWidth = 200;
+		Font font = new Font(FontFamily.HELVETICA, 4);
+		PdfContentByte cb = writer.getDirectContent();
+		BarcodeEAN codeEAN = new BarcodeEAN();
+		codeEAN.setCodeType(Barcode.UPCA);
+		
+		PdfPTable table = new PdfPTable(numCols);
+		table.setTotalWidth(numCols * colWidth);
+		
+		for (int i = 0; i < items.size(); ++i) {
+			Item item = (Item) items.get(i).getTag();
+			String title = item.getProduct().getDescription();
+			String entry = DateUtils.formatDate(item.getEntryDate());
+			String exp = expDateToString(item.getExpirationDate());
+
+			PdfPCell cell = new PdfPCell();
+			cell.setBorder(Rectangle.NO_BORDER);
+
+			codeEAN.setCode(item.getBarCode().toString());
+			cell.addElement(new Paragraph(title, font));
+			cell.addElement(new Paragraph(entry + exp, font));
+
+			Image image = codeEAN.createImageWithBarcode(cb, null, null);
+			image.setSpacingBefore(3);
+			cell.addElement(image);
+
+			table.addCell(cell);
+		}
+
+		int remainingCells = numCols - (items.size() % numCols);
+
+		for (int i = 0; i < remainingCells; i++) {
+			PdfPCell cell = new PdfPCell();
+			cell.setBorder(Rectangle.NO_BORDER);
+			table.addCell(cell);
+		}
+		
+		return table;
+	}
+	
+	private String expDateToString(Date expDate){
+		String result;
+		
+		if (expDate != null) {
+			result = " exp " + DateUtils.formatDate(expDate);
+		} else {
+			result = "";
+		}
+		
+		return result;
+	}
 }
