824c10e2c7e61f9f982a56c0a3cd8697657c4a32
David Patty <davidpatty@gmail.com>
2012-11-02 17:03:02 -0600

    changes with NMonthSupply Report

--- src/reports/directors/SupplyDirector.java
+++ src/reports/directors/SupplyDirector.java
@@ -1,16 +1,68 @@
 package reports.directors;
 
 import gui.reports.Builder;
+import gui.reports.Table;
+import gui.reports.Row;
+import java.util.List;
+import model.CoreObjectModel;
+import model.entities.Product;
+import model.managers.ProductManager;
+import reports.visitors.NMonthVisitor;
 
 public class SupplyDirector extends Director {
-
-	public SupplyDirector(Builder builder) {
+	
+	/**
+	 *  The N in NMonths
+	 */
+	int months;
+	
+	/**
+	 * 
+	 * @param builder 
+	 */
+	public SupplyDirector(Builder builder, int months) {
 		super(builder);
-		// TODO Auto-generated constructor stub
+		this.months = months;
 	}
 
-	public void createReport(int months) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
+	
+//	@Override //removed cause there is no createReport in Director
+	public void createReport() {
+		NMonthVisitor visitor = new NMonthVisitor(months);
+		
+		//Get the data by traversing the tree Pre-Order, Items sorted by 
+		//Description and entry date, ProductContainers sorted by name
+		ProductManager manager = CoreObjectModel.getInstance().getProductManager();
 
+		//visitor stores the info from where he visits 
+		manager.accept(visitor);
+		List<Product> products = visitor.getProducts();
+		
+		//Start Drawing the report
+		getBuilder().drawTitle("Expired Items");
+		
+		Table table = new Table();
+		table.addRow(getProductTitleRow());
+		for(Product product : products){
+			table.addRow(getProductRow(product));
+		}
+		
+		getBuilder().drawTable(table);
+	}
+	
+	/**
+	 * 
+	 * @return 
+	 */
+	private Row getProductTitleRow() {
+		return new Row();
+	}
+	/**
+	 * 
+	 * @param product
+	 * @return 
+	 */
+	private Row getProductRow(Product product) {
+		return new Row();
+	}
 }
