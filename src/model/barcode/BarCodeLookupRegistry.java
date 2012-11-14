538cdeb2522b6f7e915d06a24612c937056f9de1
David Patty <davidpatty@gmail.com>
2012-11-14 16:16:44 -0700

    adding files for BarcodeLookup

---
+++ src/model/barcode/BarCodeLookupRegistry.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.barcode;
+
+/**
+ *
+ * @author davidpatty
+ */
+public class BarCodeLookupRegistry {
+	
+}
